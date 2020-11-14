97678936b9a3fb6e5b721322042b995a710a0eaf
lfp1024 <lfp1024@126.com>
2020-11-14 16:57:25 +0800

    update

--- src/json.js
+++ src/json.js
@@ -2,26 +2,39 @@ const { Student, sequelize, Teacher } = require('./init')
 const { Op } = require('sequelize');
 
 async function update() {
+    // 获取出来的是一个 json 字符串，需要 parse 成 JavaScript 对象
+    // desc: '{ "proxy": { "site": { "JP": "foreign", "SA": "foreign", "SZ": "domestic" } }, "others": {} }',
     await Teacher.update(
         {
-            desc: '{"proxy":{"site":{"JP":"foreign","SA":"foreign","SZ":"domestic"}},"others":{}}',
+            desc: { "proxy": { "site": { "JP": "foreign", "SA": "foreign", "SZ": "domestic" } }, "others": {} },
         },
         {
             where: {
-                name: 'park'
+                name: 'everson'
             }
         }
     );
-
     const res = await Teacher.findOne({
         where: {
-            name: 'park'
+            name: 'everson'
         },
         attributes: ['desc']
     });
-    const obj = JSON.parse(res.desc)
-    console.log('res = ', typeof res.desc, res.desc, res.desc.proxy) // 获取出来的是一个 json 字符串，需要 parse 成 JavaScript 对象
-    console.log('res = ', typeof obj, obj, obj.proxy)
+    console.log('res = ', typeof res.desc, res.desc, res.desc.proxy) // 不加引号，取出来的是一个对象
+}
+
+
+
+async function findJson() {
+    const res = await Teacher.findOne({
+        where: {
+            name: 'duncan'
+        },
+        attributes: ['desc']
+    });
+    // const obj = JSON.parse(res.desc)
+    console.log('res = ', typeof res.desc, res, res.desc)
+    // console.log('res = ', typeof obj, obj, obj.proxy)
 }
 
 
@@ -29,4 +42,4 @@ async function update() {
     fn()
         .then(data => { console.log('operate successfully:', data) })
         .finally(() => { sequelize.close() })
-})(update)
+})(findJson)
