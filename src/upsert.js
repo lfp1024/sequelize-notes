5eb58fe5f0149bd42626d439de6e89a880f8e346
lfp1024 <lfp1024@126.com>
2020-11-26 11:41:55 +0800

    upsert

--- src/upsert.js
+++ src/upsert.js
@@ -1,7 +1,7 @@
 const { Student, sequelize } = require('./init')
 const { Op } = require('sequelize')
 
-async function upsertTest(){
+async function upsertTest() {
     let user1 = {
         name: '小七',
         age: 19,
@@ -10,8 +10,10 @@ async function upsertTest(){
         name: '小七',   // 必须要有唯一性约束，才能更新插入
         age: 190,
     }
-    await Student.create(user1);
-    await Student.upsert(user2)
+    // INSERT INTO `students` (`id`,`name`,`age`) VALUES (DEFAULT,?,?);
+    await Student.create(user1, { logging: sql => console.log(sql) });
+    // INSERT INTO `students` (`name`,`age`) VALUES (?,?) ON DUPLICATE KEY UPDATE `name`=VALUES(`name`),`age`=VALUES(`age`)
+    await Student.upsert(user2, { logging: sql => console.log(sql) })
 }
 
 
