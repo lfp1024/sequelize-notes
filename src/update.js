e95114f1d674d49887f8c96ba70f7cedbefdd1b0
lfp1024 <lfp1024@126.com>
2020-12-09 19:50:47 +0800

    test

--- src/update.js
+++ src/update.js
@@ -4,16 +4,20 @@ const { Op } = require('sequelize')
 async function update() {
     const res = await Student.update(
         {
-            age: 19,
+            age: 190,
         },
         {
             where: {
-                name: '小七'
+                name: '小七1212'
             }
         }
     );
     console.log('res =', res)
 }
+/**
+ * 查找到符合条件的记录，如果值相等，则不更新，返回受影响的个数为[0]
+ * 查找不到符合条件的记录，返回受影响的个数为[0]，
+ */
 
 
 (async (fn) => {
